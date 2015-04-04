c41bf6cc23231adb3e649b8d96e61daad101d146
Alexander Khomenko <alexanderkhomenko703@gmail.com>
2015-04-04 14:37:10 +0300

    Initial Commit

---
+++ SendPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SendPhoto
+//
+//  Created by Александр on 04.04.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
